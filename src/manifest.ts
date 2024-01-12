df6967d3617cfd7b0b93ea972b959c17753a5d68
Travis Wagner <travis.wagner@umusic.com>
2024-01-12 13:32:28 -0500

    move from json to ts for manifest

---
+++ src/manifest.ts
@@ -0,0 +1,12 @@
+const manifest: ManifestV3 = {
+    manifest_version: 3,
+    name: "Better GitHub Notifications",
+    version: "1.0",
+    background: { service_worker: "background.ts" },
+    permissions: ["identity", "storage", "alarms"],
+    content_scripts: [{ matches: ["https://github.com/*"], js: ["content.js"] }],
+    action: { default_popup: "popup.html" },
+    oauth2: { client_id: "43ce600f21d429a85512", scopes: ["user"] },
+};
+
+export default manifest;
